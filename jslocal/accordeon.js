79a9248d05713bed02f77119620d12a2de9e4558
dave-null <tim.plath@web.de>
2013-07-24 22:35:21 +0200

    Accordeon v0.1

--- jslocal/accordeon.js
+++ jslocal/accordeon.js
@@ -1,6 +1,16 @@
 function accordeonInit() {
 	$(".gebiet h2").click(function(){
-		$("#"+$(this).parent().attr("id")+' .content').slideToggle(250);
+		//$("#"+$(this).parent().attr("id")+' .content').slideToggle(250);
 		$(this).parent().toggleClass("open");
+
+		if(!$("#accordeon").hasClass("accordeonClosed")) {
+			$("#accordeon").animate({
+				width:'30px'
+			}, 450, function() {
+				$(this).addClass("accordeonClosed");
+				$('#mapInfo').html($("#"+$(this).parent().attr("id")+' .content').html());
+				$("#mapInfo").show(250);
+			});
+		}
 	});
 }
